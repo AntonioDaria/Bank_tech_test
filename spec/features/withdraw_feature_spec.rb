296da6f238b4550e69aeaa8f7c01591578cfe82b
Antonio <antoniodaria@msn.com>
2018-11-05 13:48:04 +0000

    Fixed a typo in the test, withdraw instead of deposit

--- spec/features/withdraw_feature_spec.rb
+++ spec/features/withdraw_feature_spec.rb
@@ -1,7 +1,7 @@
 describe 'feature withdraw' do
   let(:account) { Account.new }
 
-  it 'allows the user to deposit funds' do
+  it 'allows the user to withdraw funds' do
     account.deposit(1000)
     account.withdraw(500)
     expect(account.balance).to eq 500
