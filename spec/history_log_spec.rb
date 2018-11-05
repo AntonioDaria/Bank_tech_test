8dca46c6453de7fe091a9eb08c53e9b9a868b7ca
Antonio <antoniodaria@msn.com>
2018-11-05 23:58:02 +0000

    Fixed matching balance error in the log_deposit test

--- spec/history_log_spec.rb
+++ spec/history_log_spec.rb
@@ -4,10 +4,17 @@ describe History_log do
 
   describe '#log_deposit' do
     it 'adds credit info to the history log' do
-      expect(history_log.log_deposit(200, 200))
+      expect(history_log.log_deposit(300, 300))
         .to eq [:date, '05-11-2018', :balance, 300, :credit, 300]
     end
   end
+
+  # describe '#log_withdraw' do
+  #   it 'adds info about withdrawal to the history log' do
+  #     expect(history_log.log_withdraw(200, 200))
+  #       .to eq [:date, '05-11-2018', :balance, 300, :credit, 300]
+  #   end
+  # end
 
 
 end
