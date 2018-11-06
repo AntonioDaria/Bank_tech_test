0cea7240b63ad39ac017043e29b0d1c008ae7799
Antonio <antoniodaria@msn.com>
2018-11-06 12:41:23 +0000

    Refacotred print_statement feature test

--- spec/features/print_statement_feature_spec.rb
+++ spec/features/print_statement_feature_spec.rb
@@ -3,12 +3,12 @@ require 'account'
 describe "feature print statement" do
   let(:account) { Account.new}
 
-  xit "prints a statement" do
+  it "prints a statement" do
     account.deposit(1000)
     account.withdraw(300)
     format_statement = "date || credit || debit || balance\n"\
-                       "05-11-2018 ||  ||300.00  || 700.00\n"\
-                       "05-11-2018 || 1000.00 ||  || 1000.00\n"
+                       "#{Time.now.strftime('%d-%m-%Y')} ||  || 300.00 || 700.00\n"\
+                       "#{Time.now.strftime('%d-%m-%Y')} || 1000.00 ||  || 1000.00\n"
     expect{ account.show_statement }.to output(format_statement).to_stdout
   end
 end
